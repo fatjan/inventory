ced1c5ef9d7205e4280168e1ca4786def20d7545
Fatma <fatma.janna.utp@gmail.com>
2025-02-04 16:21:15 +0700

    models and db migrate

--- inventory/models.py
+++ inventory/models.py
@@ -2,7 +2,10 @@ from django.db import models
 from django.core.exceptions import ValidationError
 
 class Supplier(models.Model):
-    name = models.Charfield(max_length = 50, unique = True)
+    name = models.CharField(max_length = 50, unique = True)
+    created_at = models.DateTimeField(auto_now_add=True)  
+    updated_at = models.DateTimeField(auto_now=True)     
+
     def __str__(self):
         return self.name
 
@@ -12,12 +15,14 @@ def validate_max_length(value):
         raise ValidationError(f"Text exceeds the maximum length of {max_length} characters.")
 
 class Inventory(models.Model):
-    name = models.Charfield(max_length=50, unique=True)
-    description = models.Charfield(max_length=100, null=True)
+    name = models.CharField(max_length=50, unique=True)
+    description = models.CharField(max_length=100, null=True)
     note = models.TextField(validators=[validate_max_length])
     stock = models.PositiveIntegerField(null=False, default=0)
     availability = models.BooleanField(null=False, default=False)
     supplier = models.ForeignKey(Supplier, related_name="inventories", on_delete=models.SET_NULL, null = True)
-    
+    created_at = models.DateTimeField(auto_now_add=True)  
+    updated_at = models.DateTimeField(auto_now=True)
+
     def __str__(self):
         return self.name
